bc4e4bc2dc6ca7a5480c1dc0dfb2d13eb02c46a0
Ashley Renee Smith <ashleyreneesmith@zipcoders-MBP-2.fios-router.home>
2019-02-06 20:34:08 -0500

    my changes to this lab progect

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,65 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String result = "";
+        for (int i  = 0 ; i < stop ; i++){
+            result = result + i;
+        }
+        return result;
     }
+    
+   
 
     public static String getRange(int start, int stop) {
-        return null;
+         String result = "";
+        for (int i  = start ; i < stop ; i++){
+            result = result + i;
+        }
+        return result;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        for (int i  = start ; i < stop ; i += step){
+            result = result + i;
+        }
+        return result;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        if (start % 2 == 1){
+            start++;}
+        for (int i  = start ; i < stop ; i +=2){
+            
+            result = result + i;
+        }
+        return result;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        if (start % 2 == 0){
+            start++;}
+        for (int i  = start ; i < stop ; i +=2){
+            result = result + i;
+        }
+        return result;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+      String result = "";
+        for (int i = start ; i < stop + 1; i++){
+        
+         double answer = Math.pow(i, exponent);
+         result = result + (int) answer;
+         
+        }
+      
+      return result;
+      
     }
 }
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,41 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String result = "";
+       for(int i = 0; i < numberOfStars; i++){
+         result = result + "*";
     }
-    
+    return result ;
+}
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String triangle = "";
+        for(int i = 1; i <= numberOfRows; i++) {
+            for(int j = 0 ; j  < i ; j++) {
+                triangle = triangle + "*";
+            }
+            triangle = triangle + "\n";
     }
-
-
+    return triangle ;
+}
     public static String getSmallTriangle() {
-        return null;
+        String triangle = "";
+        for(int i = 1; i <= 4; i++) {
+            for(int j = 0 ; j  < i ; j++) {
+                triangle = triangle + "*";
+            }
+            triangle = triangle + "\n";
+    }
+    return triangle ;
     }
 
     public static String getLargeTriangle() {
-        return null;
+       String triangle = "";
+        for(int i = 1; i <= 9; i++) {
+            for(int j = 0 ; j  < i ; j++) {
+                triangle = triangle + "*";
+            }
+            triangle = triangle + "\n";
+    }
+    return triangle ;
     }
 }
